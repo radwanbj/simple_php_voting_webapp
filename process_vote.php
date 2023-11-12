8629ea1246ed131dfff04153d8463ed8d023c1fc
Radwan Wlad Elhaj <40129561+radwanbj@users.noreply.github.com>
2023-11-12 18:05:03 +0200

    Update process_vote.php

--- process_vote.php
+++ process_vote.php
@@ -30,5 +30,40 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $voter_name = $_POST['voter_name'];
+    $candidate = $_POST['candidate'];
+
+    try {
+        $sql = "INSERT INTO votes (voter_name, candidate) VALUES (?, ?)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$voter_name, $candidate]);
+        echo "Vote recorded successfully!<br>";
+
+        // Count the votes for each candidate
+        $sql = "SELECT candidate, COUNT(*) AS vote_count FROM votes GROUP BY candidate";
+        $stmt = $pdo->query($sql);
+        $totalVotes = 0;
+
+        while ($row = $stmt->fetch()) {
+            $candidate = $row['candidate'];
+            $voteCount = $row['vote_count'];
+            $totalVotes += $voteCount;
+            echo "$candidate received $voteCount votes<br>";
+        }
+
+        // Calculate and display the percentage
+        if ($totalVotes > 0) {
+            $burgerPercentage = ($voteCount / $totalVotes) * 100;
+            $pizzaPercentage = (100 - $burgerPercentage);
+            echo "Burger: $burgerPercentage%<br>";
+            echo "Pizza: $pizzaPercentage%";
+        } else {
+            echo "No votes recorded yet.";
+        }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
 // Close the database connection (PDO does this automatically when the script ends).
 ?>
