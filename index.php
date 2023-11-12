063c088d47b85a18f3b3c8d3124f87e35e7784bf
Radwan Wlad Elhaj <40129561+radwanbj@users.noreply.github.com>
2023-11-12 18:10:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -112,8 +112,21 @@
             <th>Percentage</th>
         </tr>
         <?php
-            // Include the PDO connection code here.
+            // Database configuration
+$db_host = 'node6478-taqnya-expo-demo.fin.libyanspider.cloud';
+$db_user = 'root';
+$db_password = 'VEGqcb43669';
+$db_name = 'election';
 
+
+// Connect to the database
+try {
+    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
+    // Set PDO to throw exceptions on error
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
             // Query to count votes for each candidate
             $sql = "SELECT candidate, COUNT(*) AS vote_count FROM votes GROUP BY candidate";
             $stmt = $pdo->query($sql);
