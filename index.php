dbf0c071faf23624f77db3cb450feb2c6bbb1084
Radwan Wlad Elhaj <40129561+radwanbj@users.noreply.github.com>
2023-11-12 18:08:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -88,8 +88,17 @@
         <img src="burger.jpg" alt="Burger" width="200">
         <img src="pizza.jpg" alt="Pizza" width="200">
     </div>
-    <form action="process_vote.php" method="post">
-        <!-- Your voting form -->
+   <form action="process_vote.php" method="post">
+        <label for="voter_name">Your Name:</label>
+        <input type="text" name="voter_name" required><br>
+
+        <label for="candidate">Choose an Option:</label>
+        <select name="candidate">
+            <option value="Burger">Burger</option>
+            <option value="Pizza">Pizza</option>
+        </select><br>
+
+        <input type="submit" value="Vote">
     </form>
     <div class="results">
         <h2>Results</h2>
